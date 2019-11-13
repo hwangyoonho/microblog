f4ffebc333f603f9821bc745aabf229e5bfc5885
hwangyoonho <hwangyoonho@MacBook-Pro.local>
2019-11-12 19:10:36 -0800

    Chapter10

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,7 @@ from flask_login import LoginManager
 from config import Config
 from logging.handlers import RotatingFileHandler
 import os 
+from flask_mail import Mail 
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -14,6 +15,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+mail = Mail(app)  
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
