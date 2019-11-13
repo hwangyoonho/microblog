20e13bdb1f57773df91ad2da23c2057af5ae6376
hwangyoonho <hwangyoonho@MacBook-Pro.local>
2019-11-12 18:53:19 -0800

    Chapter9

--- config.py
+++ config.py
@@ -12,3 +12,4 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['sky10081210@gmail.com']   
+    POSTS_PER_PAGE = 5 
