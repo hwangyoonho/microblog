0fc3e1cec1eae6325ff7bb42e7660319f464b2d9
hwangyoonho <hwangyoonho@MacBook-Pro.local>
2019-11-17 23:27:06 -0800

    Chapter22

--- config.py
+++ config.py
@@ -21,4 +21,5 @@ class Config(object):
     POSTS_PER_PAGE = 5   
     ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL') 
     LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')  
+    REDIS_URL = os.environ.get('REDIS_URL') or 'redis://'  
 
